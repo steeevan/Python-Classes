0ee4a5ace56ee72639cede08c4f4d8a5de4d03d9
steeevan <48275985+steeevan@users.noreply.github.com>
2023-08-01 16:36:33 -0700

    Lesson 4: Basic Operations and Expressions
    Objective: Teach arithmetic operations, comparison operators, and logical expressions in Python.
    Key Terms/Definitions: Operators, Arithmetic Operations, Comparison Operators, Logical Operators, Expressions

---
+++ Python-Basic/Code-Samples/Lesson 4.py
@@ -0,0 +1,64 @@
+# Review from last week
+# List
+
+#List of Integers
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+# List of strings
+words = ["Hello", "Goodbye", "House", " Street"]
+
+# List of booleans
+bools = [True, False, True, True]
+
+# List of floats
+floats = [1.1 , 2.1 , 3.1 , 4.1]
+
+#Print Integer 3 and 5 and 10 from the numbers list
+print(numbers[2])
+print(numbers[4])
+print(numbers[9])
+
+# Print Hello and Goodbye from the words list
+print(words[0])
+print(words[1])
+
+
+# Mixed of data types
+scramble = [100, "Yan Ming" , 2.5, True, numbers]
+print(scramble)
+
+# Add one Integer to the list Numbers
+numbers.append(5)
+#add one word to the list of words
+words.append("Sword")
+print(numbers)
+print(words)
+#--------------------------------------
+# Remove the last element of Numbers
+numbers.pop()
+# Remove the last element of words
+words.pop()
+print(numbers)
+print(words)
+
+---------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
