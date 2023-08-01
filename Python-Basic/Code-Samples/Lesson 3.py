eac9245ef8a59b8f86c15351cce562ebe6d8ecf2
steeevan <48275985+steeevan@users.noreply.github.com>
2023-08-01 16:34:48 -0700

    Lesson 3: Variables and Data Types
    Objective: Introduce the concept of variables and explore different data types in Python, such as integers, strings, floats, and booleans.
    Key Terms/Definitions: Variables, Data Types, Integers, Strings, Floats, Booleans

---
+++ Python-Basic/Code-Samples/Lesson 3.py
@@ -0,0 +1,107 @@
+total = 0
+'''
+# Ask user to enter their favorite number
+favNumber = int(input("Enter your favorite number: "))
+
+# Ask user to enter their least favorite
+leastFavNum = int(input("Enter your least favorite number: "))
+
+total = favNumber + leastFavNum
+
+print(total)
+'''
+#------------------------------------------------------------------------
+# DATA TYPES
+word = "Hello"        # string
+example2 = 10.5       # float
+integer = 100         # integer
+boolean = True        # boolean
+
+
+lst = [10, 20, 30, 40, 50]
+print(lst)
+
+print("First number to my list is: ", lst[0])
+print("Second number to my list is: ", lst[1])
+print("Third number to my list is: ", lst[2])
+print("Fourth number to my list is: ", lst[3])
+print("Fifth number to my list is: ", lst[4])
+
+# make a list that has the dice values
+dice = [1,2,3,4,5,6]
+
+print("The dice landed on:" , dice[5])
+
+
+dice.append(7)
+print(dice)
+dice.append(8)
+print(dice)
+dice.append(9)
+print(dice)
+dice.append(10)
+print(dice)
+dice.append(11)
+print(dice)
+dice.append(12)
+print(dice)
+
+print("The first roll was:" , dice[10])
+print("The second roll was:" , dice[9])
+print("The third roll was:" , dice[8])
+print("The fourth roll was:" , dice[7])
+
+
+dice.append(10)
+print(dice)
+dice.append(16)
+print(dice)
+dice.append(10)
+print(dice)
+dice.append(10)
+print(dice)
+
+
+dice.pop()
+print(dice)
+dice.pop()
+print(dice)
+dice.pop()
+print(dice)
+dice.pop()
+print(dice)
+
+
+
+
+
+
+
+# Probelm 1
+# Make a list named "years" and set it to the classmates years old
+
+# Problem 2
+# Print the youngest year old
+
+# Andy and Gerald are both 9
+
+
+
+#problem 3
+# print the oldest years old
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
