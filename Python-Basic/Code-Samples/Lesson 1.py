63a9304841d6556b43cda5e292af396a813ff133
steeevan <48275985+steeevan@users.noreply.github.com>
2023-08-01 16:29:00 -0700

    Lesson 1  Introduction to Programming
    Objective: Introduce students to the world of programming, explain the importance of programming, and familiarize them with basic programming concepts.
    Key Terms/Definitions: Programming, Algorithm, Syntax, IDE, Compiler, Interpreter  Python Interpreter, IDE

---
+++ Python-Basic/Code-Samples/Lesson 1.py
@@ -0,0 +1,68 @@
+number = 10
+
+number2 = 0.5 
+
+word = "Christmas"
+
+bolt = True
+
+print(number)
+print(number2)
+print(word)
+print(bolt)
+
+
+#arithmetic Operators 
+
+#  + , - , / , * 
+
+# aDDTION
+Sum = 105 + 5
+print(Sum)
+
+#Subtraction
+Sub = 50 - 10
+print(Sub)
+
+#Division
+
+div = 6/2
+print(div)
+div = 5 // 2
+print(div)
+
+# multiplaction
+
+mult = 5 * 5
+print(mult)
+
+
+
+
+# 1. Assign a variable named "x" - 10
+# 2. Assign a variable named "y" - 150
+# 3. Create 4 new variables that hold + , - , / , * 
+# 4. Print out the 4 variables that were created
+
+
+justin = x + y
+madelym = x - y
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
