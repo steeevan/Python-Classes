87fe7691045dc1c48cbd1babd213b5429bd45f59
AdamLiu1004 <142067700+AdamLiu1004@users.noreply.github.com>
2023-09-01 17:17:29 -0700

    Create Homework 4.py

---
+++ Python-Basic/Homework/Adam/Homework 4.py
@@ -0,0 +1,80 @@
+'''
+#Problem1
+number = 5
+int(input("Choose a number that multiples the times table: ")
+while number <= 12
+  print(number)
+number *= 1
+print(number)
+'''
+#Problem2
+L = [1,2,3,4,5,6]
+sum = 0
+for i in L:
+  if i%2 == 0:
+    sum += i
+print(sum)
+
+'''
+#Problem3
+tuple = ("Hot pot", "Barbeque","Noodles")
+print(len(tuple))
+
+C= (37,30,28)
+for i in C:
+  Fahren = C*(9/5) + 32
+print(Fahren)
+'''
+#Problem4
+number = 33
+
+if number > 33:
+  print("Lower")
+elif number < 33:
+  print("Higher")
+game = int(input("Guess a number between (1-100): "))
+while number != game:
+  print("Wrong answer")
+
+  game = int(input("Guess a number between (1-100): "))
+print("This is the right answer!")
+
+#Problem5
+list = ["Snoopy","Charlie Brown", "Winnie","Pooh"]
+print(list)
+new_list = [list(len[5])]
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+#confused: what does remove duplicates mean?
+
+#Problem6
+coordinates = {"x-coordinate":"x", "y-coordinate": "y"}
+for i in coordinates:
+  print(len(coordinates))
+  
+#Problem7
+numbers = [1-50]
+generate = numbers.random(20)
+print(generate)
+
+#Problem8
+
+num = [1,3,4,22,33,10]
+for i in num
+  print()
+product = num*num
+
+#Problem9
+int(input("How old are you: "))
+for i in range(0-12)
+  print("Child")
+for i in range(13-19)
+  print("Teenager")
+for i in range(20-65)
+  print("Adult")
+for i in range(66-100)
+  print("Old")
+  
+#Problem10
+#Help!
+
