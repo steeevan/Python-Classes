474d6cfd5ec24ebc2503ed4f516f8722df45770b
steeevan <48275985+steeevan@users.noreply.github.com>
2024-02-01 20:13:11 -0800

    Lesson 1 Intro to Programming

---
+++ Python Basic Class Th 6:30pm:8:00pm/Lessons /Lesson1.py
@@ -0,0 +1,101 @@
+# Programming is like giving instructions to a robot or a computer
+# Just like how you might give directions to a friend to bake a cake
+# or play a game
+
+# What is a variable?
+# A variable is like a box or a container that holds information. A variable 
+# must have a name and it can hold different types of information
+# such as : numbers, words or more complex things
+
+
+# Make a variable
+number = 10
+
+number = "grace"
+
+
+#Valid Variable Names
+# - Must start with a letter (a-z, A-Z) or an underscore(_)
+# ex. 'my_variable', '_variable1', 'Name'
+# - Can contain letters, numbers(0-9) or underscores
+# ex. 'My_var_var_123', name123456789'
+# Variables are case-sensitive
+# ex. 'myVariable' , 'MyVariable', myvariable'
+# there are all different variables
+# - Cannot be usign a reserved keyworkd in Python
+# ex. if, else, for, def, return, etc.
+
+
+# Invalid Variables
+# - Cannont start with a number
+# ex. INVALID: '2variable', '123'
+# - Cannot contain any special character(except underscore)
+# ex. 'my-variable', 'my@variable', ' my!variable'
+# - cannot contain spaces
+# ex. 'my variable', 'myVariable I like'
+
+
+# What is Syntax?
+# Syntax is like the grammer or rules of a language. Python has its
+# own set of rules that you need to follow to write code correctly
+
+# Exercises
+# Assignment operator '=' assigns a value to your variable
+
+number = 5
+number2 = 1.5
+
+word = "happy"
+Word = "sad"
+
+print(number)
+print(number2)
+print(word)
+print(Word)
+
+
+# Arithmetic Operators
+# Addition
+SUM = number + number 
+print(SUM)
+
+#Subtraction
+diff = number - number2
+print(diff)
+
+#Multiplicatin
+mult = number * number2
+
+#Dvision
+div = number / number2
+
+
+# Invalid operation
+SUM = number + word
+# Explain why? 
+
+
+
+# 1. Assign a variable named "x" - 10
+x = 10
+# 2. Assign a variable named "y" - 150
+
+# 3. Create 4 new variables that hold + , - , / , * 
+mySum = x + y
+
+# 4. Print out the 4 variables that were created
+print(mySum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
