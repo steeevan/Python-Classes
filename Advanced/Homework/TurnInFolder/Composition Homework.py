425fc22e2279972aeef0e860e890f6429e0df697
Plexer81 <141675090+Plexer81@users.noreply.github.com>
2024-02-06 16:35:53 -0800

    Composition Part 2 Yanming Hwang

---
+++ Advanced/Homework/TurnInFolder/Composition Homework.py
@@ -0,0 +1,7 @@
+from Main import Computer, Monitor, Mouse
+
+def computer():
+    computer1 = Computer("AMD Ryzen 5",128,256)
+    monitor1 = Monitor("Medium","7x1000")
+def mouse():
+    mouse1 = mouse("small","red","sensitive")
