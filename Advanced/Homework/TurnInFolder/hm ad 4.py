a0fe4398e98edefaeba35f129999c1fe7faa20e4
jUsTiN2569 <145215814+jUsTiN2569@users.noreply.github.com>
2024-03-07 21:37:23 -0800

    hm 4
    hm 4

---
+++ Advanced/Homework/TurnInFolder/hm ad 4.py
@@ -0,0 +1,56 @@
+class Computer:
+    def __init__(self, cpu, ram, storage):
+        self.cpu = cpu
+        self.ram=ram
+        self.storage = storage
+
+    
+    def info(self):
+        print (f"cpu = {self.cpu} ram = {sellf.ram} GB storage = {self.storage} GB")
+
+computer1 = Computer("CPU", 1432, 98631250)
+computer1.info
+class moniter:
+    def __init__(self,size,resulution):
+        self.size = size
+        self.resulution = resulution
+
+    def Moniter_info(self):
+        print(f" The moniter size is {self.size} IN and resulution is {self.resulution} IN")
+moniter1=moniter("12", "678326142873946218739")
+moniter1.Moniter_info
+class desktop:
+    def __init__(self, Computer, moniter):
+        self.Computer = Computer
+        self.moniter=moniter
+
+    def Desktop_info(self):
+        print(f" cpu = {self.cpu} ram = {sellf.ram} GB storage = {self.storage} GB, The moniter size is {self.size} IN and resulution is {self.resulution} IN")
+
+desktop1=desktop("CPU", 632187, 96143,"31321", "671832964187329")
+desktop1.Desktop_info
+
+
+
+
+
+#------------------------------------------------------------
+class num:
+    def __init__(self, num):
+        self.num1 = num1
+
+class combining_num:
+    def __init__(self, num2):
+        self.num2 = num2
+
+class combined_num:
+    def __init__(self, num, combining_num)
+        self.num = num
+        self.combining_num = combining_num
+
+    def combined_Num(self):
+        print(f"the combined num = {self.num + self.combining_num")
+
+
+
+        #idk if work
