5181e8f3f90bfb82764f300e976a58ff9653d19e
JohnathonBao <163490447+JohnathonBao@users.noreply.github.com>
2024-03-15 17:49:34 -0700

    Hw1.py

---
+++ Python Basic Class Th 6:30pm:8:00pm/Homework/Johnathon/hw1.py
@@ -0,0 +1,22 @@
+#part 1
+#1:
+# variable: letters or symbols that stand for something, but can change depending on the code.
+# syntax: a ruleset for a language
+# agorithm: a code or strategy for something
+# IDE: a tool for creating and running python
+# complier: a program that translates python into another language
+# interpreter: a program that does instructions written in python.
+
+#2: compliers traslate python to another language, but interpreters run the the code.
+
+#3 Make a python file, write the code, and run it.
+
+#4
+num1=3
+num2=5
+num3=1
+num4=6
+num5=9
+
+if num5==num4+num1:
+  print (num1,"+", num4,"=", num5)
