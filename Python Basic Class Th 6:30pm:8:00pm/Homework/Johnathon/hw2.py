3f287c8b7048a3aa9122d6eb02bc9f56d9797303
JohnathonBao <163490447+JohnathonBao@users.noreply.github.com>
2024-03-15 18:48:16 -0700

    hw2.py

---
+++ Python Basic Class Th 6:30pm:8:00pm/Homework/Johnathon/hw2.py
@@ -0,0 +1,30 @@
+#1
+
+age=12
+name="Johnathon Bao"
+temp="70 degrees"
+weather="sunny"
+
+#2
+
+print ("I am", age, "years old, and my name is", name,". The temperature is", temp,"so it is", weather,"outside.")
+
+#3
+#Integer: any number
+#string: any sequence of numbers or letters
+#float: a function that turnes values into floating numbers
+#boolean: something that can only haev true or false.
+
+#4
+
+age=(input("How old are you?"))
+
+#5
+
+Cdegrees=(input("What is the weather ouside in Celsius"))
+
+#6 
+
+print ("You are", age, "years old.")
+print ("The temperature in Celcius is", Cdegrees,"degrees.")
+
