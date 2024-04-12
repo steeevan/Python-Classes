469537d2cbb2b21aac45b01a687d8239ae66494b
steeevan <48275985+steeevan@users.noreply.github.com>
2024-04-11 20:09:06 -0700

    Homework 5 List and tuples

---
+++ Python Basic Class Th 6:30pm:8:00pm/Homework/Zaki/homework5.py
@@ -0,0 +1,30 @@
+# 1 a list is a thing that contians thing. 
+# a tupple is a thing that contain infromation but cannot be changed but can be conbined
+# a indexing is when changing some inside the code
+# a mutability is a is a object that can be changed
+# a immutabililty is when something can't be changed
+# 2 a list is something u can change and a tuple cannot be changed
+# 3 so basicly a indexing in lists and tuple is like see if the are there
+ 
+# 4
+intergers = ["10", "20", "30"]
+intergers.append("40")
+intergers.remove("30")
+print (intergers)
+
+# 5 
+turples_names = ("Med", "Hed", "Led")
+turples_age = ("7","14","12","5")
+print(turples_age)
+print(turples_names)
+if "Med"in turples_names:
+  print("Med is in")
+else:
+  print("Med is not in")
+turples_names[2]="cap"
+#6
+# firstly, i would use list for remenbering or least important thing.
+# then lists you can change some i won"t need to be that careful if i make a mistake.
+# but then tuples i would use an important thing so u can"t change the stuff and others.
+# next, but you can still combine turples so when i use list I can change so mistakes are fine but if i use turples it maybe more inportant.
+# the similartity is both contian and act same but tuples you can't change.
