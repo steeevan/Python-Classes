fa4e525b706ec1f731e0a4407f409cc0317c20ff
steeevan <48275985+steeevan@users.noreply.github.com>
2024-04-18 19:32:50 -0700

    ToDoList Project

---
+++ Python Basic Class Th 6:30pm:8:00pm/Lessons /ToDoList.py
@@ -0,0 +1,20 @@
+def display_menu():
+  print("To-Do List Manager")
+  print("1. Add Task")
+  print("2. Remove Task")
+  print("3. View Task")
+  print("4. Exit")
+
+def add_task(tasks):
+  task = input("Enter the task: ")
+  tasks.append(task)
+  print("Task added successfully!")
+
+def remove_task(tasks):
+  task_pos = int(input("Enter the index of the task to remove: "))
+  if 1<= task_pos <= len(tasks):
+    removed_task = tasks.pop(task_index - 1)
+    print(f"Task '{removed_task}' removed successfully!")
+  else:
+    print("Invalid Position Entered!")
+    
