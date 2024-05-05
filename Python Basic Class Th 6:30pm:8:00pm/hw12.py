6bf62bb624a7a63240fbd1eed3f63247d6ee1678
JohnathonBao <163490447+JohnathonBao@users.noreply.github.com>
2024-05-05 11:43:26 -0700

    Create hw12.py

---
+++ Python Basic Class Th 6:30pm:8:00pm/hw12.py
@@ -0,0 +1,29 @@
+#1
+
+import datetime
+a=datetime.datetime.now()
+print (a)
+
+#2
+
+import random
+print (random.randint(1,11))
+
+#3
+
+import math
+a=math.sqrt(int(input("enter a number for its square root")))
+print (a)
+
+#6
+'pip install matplotlib'
+
+#7...I had help with this one
+import matplotlib.pyplot as plt
+import numpy
+x=numpy.array([0, 6])
+y=numpy.array([0, 250])
+plt.plot(x, y)
+plt.show()
+
+
