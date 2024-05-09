c3ff19c7c1d263d05d35a295b3bd862c8bb67b5b
JohnathonBao <163490447+JohnathonBao@users.noreply.github.com>
2024-05-08 19:48:20 -0700

    hw12.py

---
+++ Python Basic Class Th 6:30pm:8:00pm/Homework/Johnathon/hw12.py
@@ -0,0 +1,40 @@
+#1-3
+
+import datetime
+date=datetime.datetime.now()
+print (date)
+
+import random
+a=random.randint(1, 10)
+print (a)
+
+import math
+
+square_roots=int(input("What is your cell phone number?"))
+print (f"okay here is the square root of your cell phone number: {math.sqrt(square_roots)}")
+
+#6-7
+
+'pip install matplotlib'
+
+import matplotlib.pyplot as plt
+ 
+
+x = [1,2,3]
+
+y = [2,4,1]
+ 
+
+plt.plot(x, y)
+ 
+
+plt.xlabel("Barack Obama")
+plt.ylabel("Johnson-Johnson")
+ 
+
+plt.title("Bing chilling")
+ 
+
+plt.show()
+
+
