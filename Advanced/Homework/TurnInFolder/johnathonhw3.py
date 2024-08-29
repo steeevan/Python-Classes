eb66347c8c724b698c884de133ebdbd17e7b6166
JohnathonBao <163490447+JohnathonBao@users.noreply.github.com>
2024-08-28 18:58:18 -0700

    johnathonhw3.py

---
+++ Advanced/Homework/TurnInFolder/johnathonhw3.py
@@ -0,0 +1,39 @@
+class bankaccount:
+    def __init__(self, accountnum, balance):
+        self.accountnum=accountnum
+        self.balance=balance
+    def get_balance(self, balance):
+        self.balance.get()
+import abc
+class vehicle(ABC):
+    def __init__(self):
+        def start_engine(self):
+            print ('starting engine')
+        def stop_engine(self):
+            print ('engine stopped')
+        def drive(self):
+            print ('car is now driving')
+
+class car:
+    def __init__(vehicle):
+        def start_engine(self):
+            print ('starting engine')
+        def stop_engine(self):
+            print ('engine stopped')
+        def drive(self):
+            print ('car is now driving')
+
+
+'''
+A real world scenario could be when you are training for a
+driver's liscense.
+'''
+
+
+
+
+
+
+
+
+
